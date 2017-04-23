abdc8d924e044c2e12ad8f47970acbe9247e2440
ninetech\ksdmano <mattias.norell@ninetech.com>
2017-04-23 11:13:26 +0200

    Added status messages

--- www/src/app/components/current-status/current-status.component.ts
+++ www/src/app/components/current-status/current-status.component.ts
@@ -10,11 +10,23 @@ import {CurrentStatus } from './current-status.model';
 })
 
 export class CurrentStatusComponent implements OnInit{
-	status: String = "Laddar...";
+	status: Number = -1;
 
+	message: String[] = [
+		"Laddar",
+		"Ute och kör"
+	];
+
+	image: String[] = [
+		"https://thumbs.dreamstime.com/z/automatic-mower-modern-lawn-docking-station-charging-battery-its-self-mowing-device-image-taken-outside-garden-40225783.jpg",
+		"http://husqvarnacdn.azureedge.net//qs_h=720&crop=1&w=720&ver=00000000T000000/_$$_/media/dam/husqvarna/garden%20lawnmowers%20and%20ride-on%20mowers/robotic%20lawnmowers/2014/12/08/20/35/h350-0237.ashx"
+	];
+	
 	constructor(private currentStatusService: CurrentStatusService){ }
 
 	ngOnInit(): void{
-		this.currentStatusService.getStatus().then(response => 	this.status = response.status);
+		this.currentStatusService.getStatus().then(response => 	{
+			this.status = response.status;
+		});
 	}
 } 
--- www/src/app/components/current-status/current-status.model.ts
+++ www/src/app/components/current-status/current-status.model.ts
@@ -1,3 +1,3 @@
 export class CurrentStatus {
-  status: string;
+  status: Number;
 }
