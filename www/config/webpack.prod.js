f9c93ee7c3b4d480122fcc9f8c6742f240cec983
ninetech\ksdmano <mattias.norell@ninetech.com>
2017-05-04 23:55:54 +0200

    Added missing config files

--- www/config/webpack.prod.js
+++ www/config/webpack.prod.js
@@ -11,6 +11,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
+    publicPath: '',
     filename: '[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
